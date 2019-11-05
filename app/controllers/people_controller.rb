2825cdf69d6a33785efb9c5cff9df7f0dcfad8d5
github username <github email address>
2019-11-05 11:12:58 -0700

    Set up controllers

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,34 @@
 class PeopleController < ApplicationController
+    def index
+        people = Person.all
+        render json: people
+    end
+
+    def show
+        person = People.find(params[:id])
+        render json: person
+    end
+
+    def create
+        person = People.create({
+            name: params[:name],
+            picture: params[:picture]
+        })
+        render json: person
+    end
+
+    def destroy
+        person = People.find(params[:id])
+        person.destroy
+        render json: person
+    end
+
+    def update
+        person = People.find(params[:id])
+        person.update({
+            name: params[:name],
+            picture: params[:picture]
+        })
+        render json: person
+    end
 end
