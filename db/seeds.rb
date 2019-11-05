1650989400298b565ba4121042964fd53218a04f
Amy Nguyen <amy.nguyen730@gmail.com>
2019-11-05 10:25:05 -0700

    added rest client into seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'rest-client'
+require 'json'
+url = RestClient.get("https://quote-garden.herokuapp.com/quotes/random")
+quotes = JSON.parse(url)
+
